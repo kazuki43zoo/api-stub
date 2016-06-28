83b9bb0969fbd16b62d928a6fe874425cf0aa72d
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-06-29 03:10:07 +0900

    Add redirect setting

--- api-stub-core/src/main/java/com/kazuki43zoo/config/ApiStubConfig.java
+++ api-stub-core/src/main/java/com/kazuki43zoo/config/ApiStubConfig.java
@@ -3,13 +3,20 @@ package com.kazuki43zoo.config;
 import org.h2.server.web.DbStarter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class ApiStubConfig {
+public class ApiStubConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     DbStarter dbStarter() {
         return new DbStarter();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/manager/mocks");
     }
 
 }
