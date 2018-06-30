b054cfa5c9b0690c10119197c1a041816d3eec34
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-06-30 20:01:03 +0900

    Reformat

--- api-stub-core/src/main/groovy/com/kazuki43zoo/domain/repository/ApiRepository.groovy
+++ api-stub-core/src/main/groovy/com/kazuki43zoo/domain/repository/ApiRepository.groovy
@@ -26,173 +26,176 @@ import org.springframework.util.StringUtils
 @CacheNamespace
 interface ApiRepository {
 
-    @SelectProvider(type = SqlProvider.class, method = "findPage")
-    @Options(useCache = false)
-    List<Api> findPage(
-            @Param("path") String path, @Param("method") String method, @Param("description") String description, RowBounds rowBounds)
+  @SelectProvider(type = SqlProvider.class, method = "findPage")
+  @Options(useCache = false)
+  List<Api> findPage(
+      @Param("path") String path,
+      @Param("method") String method, @Param("description") String description, RowBounds rowBounds)
 
-    @Select('''
-        SELECT
-            id, path
-        FROM
-            mock_api
-        WHERE
-            method = #{method}
-        ORDER BY
-            path
-    ''')
-    List<Api> findAllByMethod(@Param("method") String method)
+  @Select('''
+    SELECT
+        id, path
+    FROM
+        mock_api
+    WHERE
+        method = #{method}
+    ORDER BY
+        path
+  ''')
+  List<Api> findAllByMethod(@Param("method") String method)
 
-    @SelectProvider(type = SqlProvider.class, method = "count")
-    @Options(useCache = false)
-    long count(
-            @Param("path") String path, @Param("method") String method, @Param("description") String description)
+  @SelectProvider(type = SqlProvider.class, method = "count")
+  @Options(useCache = false)
+  long count(
+      @Param("path") String path, @Param("method") String method, @Param("description") String description)
 
-    @Select('''
-        SELECT
-            a.id, a.path, a.method, a.key_extractor, a.key_generating_strategy, a.expressions, a.description
-            , SELECT COUNT(r.id) FROM mock_api_response r
-                  WHERE r.path = a.path AND r.method = a.method AS keyed_response_number
-            , p.enabled, p.url, p.capturing
-        FROM
-            mock_api a
-        LEFT OUTER JOIN
-            api_proxy p ON p.id = a.id
-        WHERE
-            a.id = #{id}
-    ''')
-    @Results([
-            @Result(property = "proxy.enabled" ,column = "enabled")
-            , @Result(property = "proxy.url" ,column = "url")
-            , @Result(property = "proxy.capturing" ,column = "capturing")
-    ])
-    Api findOne(int id)
+  @Select('''
+    SELECT
+      a.id, a.path, a.method, a.key_extractor, a.key_generating_strategy, a.expressions, a.description
+      , SELECT COUNT(r.id) FROM mock_api_response r
+            WHERE r.path = a.path AND r.method = a.method AS keyed_response_number
+      , p.enabled, p.url, p.capturing
+    FROM
+      mock_api a
+    LEFT OUTER JOIN
+      api_proxy p ON p.id = a.id
+    WHERE
+      a.id = #{id}
+  ''')
+  @Results([
+      @Result(property = "proxy.enabled", column = "enabled")
+      , @Result(property = "proxy.url", column = "url")
+      , @Result(property = "proxy.capturing", column = "capturing")
+  ])
+  Api findOne(int id)
 
-    @Select('''
-        SELECT
-            a.id, a.path, a.method, a.key_extractor, a.key_generating_strategy, a.expressions, a.description
-            , p.enabled, p.url, p.capturing
-        FROM
-            mock_api a
-        LEFT OUTER JOIN
-            api_proxy p ON p.id = a.id
-        WHERE
-            a.path = #{path}
-        AND
-            a.method = UPPER(#{method})
-    ''')
-    @Results([
-        @Result(property = "proxy.enabled" ,column = "enabled")
-        , @Result(property = "proxy.url" ,column = "url")
-        , @Result(property = "proxy.capturing" ,column = "capturing")
-    ])
-    Api findOneByUk(@Param("path") String path, @Param("method") String method)
+  @Select('''
+    SELECT
+      a.id, a.path, a.method, a.key_extractor, a.key_generating_strategy, a.expressions, a.description
+      , p.enabled, p.url, p.capturing
+    FROM
+      mock_api a
+    LEFT OUTER JOIN
+      api_proxy p ON p.id = a.id
+    WHERE
+      a.path = #{path}
+    AND
+      a.method = UPPER(#{method})
+  ''')
+  @Results([
+      @Result(property = "proxy.enabled", column = "enabled")
+      , @Result(property = "proxy.url", column = "url")
+      , @Result(property = "proxy.capturing", column = "capturing")
+  ])
+  Api findOneByUk(@Param("path") String path, @Param("method") String method)
 
-    @Select('''
-        SELECT
-            id
-        FROM
-            mock_api
-        WHERE
-            path = #{path}
-        AND
-            method = UPPER(#{method})
-    ''')
-    @Options(useCache = false)
-    Integer findIdByUk(@Param("path") String path, @Param("method") String method)
+  @Select('''
+    SELECT
+      id
+    FROM
+      mock_api
+    WHERE
+      path = #{path}
+    AND
+      method = UPPER(#{method})
+  ''')
+  @Options(useCache = false)
+  Integer findIdByUk(@Param("path") String path, @Param("method") String method)
 
-    @Insert('''
-        INSERT INTO mock_api
-            (
-                path, method, key_extractor, key_generating_strategy, expressions, description
-            )
-        VALUES
-            (
-                #{path}, UPPER(#{method}), #{keyExtractor}, #{keyGeneratingStrategy}, #{expressions}, #{description}
-            )
-    ''')
-    void create(Api api)
+  @Insert('''
+    INSERT INTO mock_api
+      (
+        path, method, key_extractor, key_generating_strategy, expressions, description
+      )
+    VALUES
+      (
+        #{path}, UPPER(#{method}), #{keyExtractor}, #{keyGeneratingStrategy}, #{expressions}, #{description}
+      )
+  ''')
+  void create(Api api)
 
-    @Insert('''
-        INSERT INTO api_proxy
-            (
-                id, enabled, url, capturing
-            )
-        VALUES
-            (
-                #{id}, IFNULL(#{proxy.enabled},false), #{proxy.url}, IFNULL(#{proxy.capturing},false)
-            )
-    ''')
-    void createProxy(Api api)
+  @Insert('''
+    INSERT INTO api_proxy
+      (
+        id, enabled, url, capturing
+      )
+    VALUES
+      (
+        #{id}, IFNULL(#{proxy.enabled},false), #{proxy.url}, IFNULL(#{proxy.capturing},false)
+      )
+  ''')
+  void createProxy(Api api)
 
-    @Update('''
-        UPDATE mock_api
-        SET
-            description = #{description}, key_extractor = #{keyExtractor}
-            , key_generating_strategy = #{keyGeneratingStrategy}, expressions = #{expressions}
-        WHERE
-            id = #{id}
-    ''')
-    void update(Api newApi)
+  @Update('''
+    UPDATE mock_api
+    SET
+      description = #{description}, key_extractor = #{keyExtractor}
+      , key_generating_strategy = #{keyGeneratingStrategy}, expressions = #{expressions}
+    WHERE
+      id = #{id}
+  ''')
+  void update(Api newApi)
 
-    @Update('''
-        UPDATE api_proxy
-        SET
-            enabled = IFNULL(#{proxy.enabled},false), url = #{proxy.url}, capturing = IFNULL(#{proxy.capturing},false)
-        WHERE
-            id = #{id}
-    ''')
-    boolean updateProxy(Api newApi)
+  @Update('''
+    UPDATE api_proxy
+    SET
+      enabled = IFNULL(#{proxy.enabled},false), url = #{proxy.url}, capturing = IFNULL(#{proxy.capturing},false)
+    WHERE
+      id = #{id}
+  ''')
+  boolean updateProxy(Api newApi)
 
-    @Delete('''
-        DELETE FROM
-           mock_api
-        WHERE
-            id = #{id}
-    ''')
-    void delete(int id)
+  @Delete('''
+    DELETE FROM
+     mock_api
+    WHERE
+      id = #{id}
+  ''')
+  void delete(int id)
 
-    @Delete('''
-        DELETE FROM
-           api_proxy
-        WHERE
-            id = #{id}
-    ''')
-    void deleteProxy(int id)
+  @Delete('''
+    DELETE FROM
+     api_proxy
+    WHERE
+      id = #{id}
+  ''')
+  void deleteProxy(int id)
 
-    static class SqlProvider {
-        public String findPage(
-                @Param("path") String path, @Param("method") String method, @Param("description") String description) {
-            return new SQL() {
-                {
-                    SELECT("a.id", "a.path", "a.method", "a.description", "SELECT COUNT(r.id) FROM mock_api_response r WHERE r.path = a.path AND r.method = a.method AND r.data_key NOT IN ('', 'default') AS keyed_response_number")
-                    FROM("mock_api a")
-                    where(this, path, method, description)
-                    ORDER_BY("a.path", "a.method")
-                }
-            }.toString()
+  static class SqlProvider {
+    public String findPage(
+        @Param("path") String path, @Param("method") String method, @Param("description") String description) {
+      return new SQL() {
+        {
+          SELECT("a.id", "a.path", "a.method", "a.description", "SELECT COUNT(r.id) FROM mock_api_response r WHERE r.path = a.path AND r.method = a.method AND r.data_key NOT IN ('', 'default') AS keyed_response_number")
+          FROM("mock_api a")
+          where(this, path, method, description)
+          ORDER_BY("a.path", "a.method")
         }
-        public String count(
-                @Param("path") String path, @Param("method") String method, @Param("description") String description) {
-            return new SQL() {
-                {
-                    SELECT("COUNT(*)")
-                    FROM("mock_api a")
-                    where(this, path, method, description)
-                }
-            }.toString()
-        }
-        private static void where(SQL sql, String path, String method, String description) {
-            if (StringUtils.hasLength(path)) {
-                sql.WHERE("a.path REGEXP #{path}")
-            }
-            if (StringUtils.hasLength(method)) {
-                sql.WHERE("a.method = UPPER(#{method})")
-            }
-            if (StringUtils.hasLength(description)) {
-                sql.WHERE("a.description REGEXP #{description}")
-            }
-        }
+      }.toString()
     }
+
+    public String count(
+        @Param("path") String path, @Param("method") String method, @Param("description") String description) {
+      return new SQL() {
+        {
+          SELECT("COUNT(*)")
+          FROM("mock_api a")
+          where(this, path, method, description)
+        }
+      }.toString()
+    }
+
+    private static void where(SQL sql, String path, String method, String description) {
+      if (StringUtils.hasLength(path)) {
+        sql.WHERE("a.path REGEXP #{path}")
+      }
+      if (StringUtils.hasLength(method)) {
+        sql.WHERE("a.method = UPPER(#{method})")
+      }
+      if (StringUtils.hasLength(description)) {
+        sql.WHERE("a.description REGEXP #{description}")
+      }
+    }
+  }
 
 }
--- api-stub-core/src/main/groovy/com/kazuki43zoo/domain/repository/ApiResponseRepository.groovy
+++ api-stub-core/src/main/groovy/com/kazuki43zoo/domain/repository/ApiResponseRepository.groovy
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2016-2017 the original author or authors.
+ *    Copyright 2016-2018 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -25,209 +25,212 @@ import org.springframework.util.StringUtils
 @Mapper
 interface ApiResponseRepository {
 
-    @SelectProvider(type = SqlProvider.class, method = "findPage")
-    List<ApiResponse> findPage(
-            @Param("path") String path, @Param("method") String method, @Param("description") String description, RowBounds rowBounds)
+  @SelectProvider(type = SqlProvider.class, method = "findPage")
+  List<ApiResponse> findPage(
+      @Param("path") String path,
+      @Param("method") String method, @Param("description") String description, RowBounds rowBounds)
 
-    @SelectProvider(type = SqlProvider.class, method = "count")
-    long count(@Param("path") String path, @Param("method") String method, @Param("description") String description)
+  @SelectProvider(type = SqlProvider.class, method = "count")
+  long count(@Param("path") String path, @Param("method") String method, @Param("description") String description)
 
-    @Select('''
-        SELECT
-            h.id, h.sub_id, o.path, o.method, o.data_key, h.status_code, h.description, h.created_at
-        FROM
-            mock_api_response_history h
-        INNER JOIN
-            mock_api_response o ON o.id = h.id
-        WHERE
-            h.id = #{id}
-        ORDER BY
-            h.sub_id DESC
-    ''')
-    List<ApiResponse> findPageHistoryById(int id, RowBounds rowBounds)
+  @Select('''
+    SELECT
+      h.id, h.sub_id, o.path, o.method, o.data_key, h.status_code, h.description, h.created_at
+    FROM
+      mock_api_response_history h
+    INNER JOIN
+      mock_api_response o ON o.id = h.id
+    WHERE
+      h.id = #{id}
+    ORDER BY
+      h.sub_id DESC
+  ''')
+  List<ApiResponse> findPageHistoryById(int id, RowBounds rowBounds)
 
-    @Select('''
-        SELECT
-            COUNT(*)
-        FROM
-            mock_api_response_history h
-        INNER JOIN
-            mock_api_response o ON o.id = h.id
-        WHERE
-            h.id = #{id}
-    ''')
-    long countHistoryById(int id)
+  @Select('''
+    SELECT
+      COUNT(*)
+    FROM
+      mock_api_response_history h
+    INNER JOIN
+      mock_api_response o ON o.id = h.id
+    WHERE
+      h.id = #{id}
+  ''')
+  long countHistoryById(int id)
 
-    @Select('''
-        SELECT
-            id, path, method, data_key, status_code, header, body, body_editor_mode
-            , attachment_file, file_name, waiting_msec, description
-        FROM
-            mock_api_response
-        WHERE
-            path = #{path}
-        AND
-            method = UPPER(#{method})
-        AND
-            data_key = IFNULL(#{dataKey},'')
-    ''')
-    ApiResponse findOneByUk(
-            @Param("path") String path, @Param("method") String method, @Param("dataKey") String dataKey)
+  @Select('''
+    SELECT
+      id, path, method, data_key, status_code, header, body, body_editor_mode
+      , attachment_file, file_name, waiting_msec, description
+    FROM
+      mock_api_response
+    WHERE
+      path = #{path}
+    AND
+      method = UPPER(#{method})
+    AND
+      data_key = IFNULL(#{dataKey},'')
+  ''')
+  ApiResponse findOneByUk(
+      @Param("path") String path, @Param("method") String method, @Param("dataKey") String dataKey)
 
-    @Select('''
-        SELECT
-            id
-        FROM
-            mock_api_response
-        WHERE
-            path = #{path}
-        AND
-            method = UPPER(#{method})
-        AND
-            data_key = IFNULL(#{dataKey},'')
-    ''')
-    Integer findIdByUk(
-            @Param("path") String path, @Param("method") String method, @Param("dataKey") String dataKey)
+  @Select('''
+    SELECT
+      id
+    FROM
+      mock_api_response
+    WHERE
+      path = #{path}
+    AND
+      method = UPPER(#{method})
+    AND
+      data_key = IFNULL(#{dataKey},'')
+  ''')
+  Integer findIdByUk(
+      @Param("path") String path, @Param("method") String method, @Param("dataKey") String dataKey)
 
-    @Select('''
-        SELECT
-            id, path, method, data_key, status_code, header, body, body_editor_mode
-            , attachment_file, file_name, waiting_msec, description
-            , SELECT COUNT(sub_id) FROM mock_api_response_history WHERE id = #{id} AS historyNumber
-        FROM
-            mock_api_response
-        WHERE
-            id = #{id}
-    ''')
-    ApiResponse findOne(int id)
+  @Select('''
+    SELECT
+      id, path, method, data_key, status_code, header, body, body_editor_mode
+      , attachment_file, file_name, waiting_msec, description
+      , SELECT COUNT(sub_id) FROM mock_api_response_history WHERE id = #{id} AS historyNumber
+    FROM
+      mock_api_response
+    WHERE
+      id = #{id}
+  ''')
+  ApiResponse findOne(int id)
 
-    @Select('''
-        SELECT
-            h.id, h.sub_id, o.path, o.method, o.data_key, h.status_code, h.header, h.body, h.body_editor_mode
-            , h.attachment_file, h.file_name, h.waiting_msec, h.description, h.created_at
-        FROM
-            mock_api_response_history h
-        INNER JOIN
-            mock_api_response o ON o.id = h.id
-        WHERE
-            h.id = #{id} AND h.sub_id = #{subId}
-    ''')
-    ApiResponse findHistory(@Param("id") int id, @Param("subId") int subId)
+  @Select('''
+    SELECT
+      h.id, h.sub_id, o.path, o.method, o.data_key, h.status_code, h.header, h.body, h.body_editor_mode
+      , h.attachment_file, h.file_name, h.waiting_msec, h.description, h.created_at
+    FROM
+      mock_api_response_history h
+    INNER JOIN
+      mock_api_response o ON o.id = h.id
+    WHERE
+      h.id = #{id} AND h.sub_id = #{subId}
+  ''')
+  ApiResponse findHistory(@Param("id") int id, @Param("subId") int subId)
 
-    @Insert('''
-        INSERT INTO mock_api_response
-            (
-                path, method, data_key, status_code, header, body, body_editor_mode
-                , attachment_file, file_name, waiting_msec, description
-            )
-        VALUES
-            (
-                #{path}, UPPER(#{method}), IFNULL(#{dataKey},''), #{statusCode}, #{header}, #{body}, #{bodyEditorMode}
-                , #{attachmentFile}, #{fileName}, #{waitingMsec}, #{description}
-            )
-    ''')
-    void create(ApiResponse mockResponse)
+  @Insert('''
+    INSERT INTO mock_api_response
+      (
+        path, method, data_key, status_code, header, body, body_editor_mode
+        , attachment_file, file_name, waiting_msec, description
+      )
+    VALUES
+      (
+        #{path}, UPPER(#{method}), IFNULL(#{dataKey},''), #{statusCode}, #{header}, #{body}, #{bodyEditorMode}
+        , #{attachmentFile}, #{fileName}, #{waitingMsec}, #{description}
+      )
+  ''')
+  void create(ApiResponse mockResponse)
 
-    @Insert('''
-        INSERT INTO mock_api_response_history
-            (
-                id, sub_id, status_code, header, body, body_editor_mode
-                , attachment_file, file_name, waiting_msec, description, created_at
-            )
-        SELECT
-            id, (SELECT IFNULL(MAX(sub_id), 0) + 1 FROM mock_api_response_history WHERE id = #{id})
-            , status_code, header, body, body_editor_mode, attachment_file, file_name, waiting_msec
-            , description, CURRENT_TIMESTAMP
-        FROM
-            mock_api_response
-        WHERE
-            id = #{id}
-    ''')
-    void createHistory(int id)
+  @Insert('''
+    INSERT INTO mock_api_response_history
+      (
+        id, sub_id, status_code, header, body, body_editor_mode
+        , attachment_file, file_name, waiting_msec, description, created_at
+      )
+    SELECT
+      id, (SELECT IFNULL(MAX(sub_id), 0) + 1 FROM mock_api_response_history WHERE id = #{id})
+      , status_code, header, body, body_editor_mode, attachment_file, file_name, waiting_msec
+      , description, CURRENT_TIMESTAMP
+    FROM
+      mock_api_response
+    WHERE
+      id = #{id}
+  ''')
+  void createHistory(int id)
 
-    @Insert('''
-        INSERT INTO api_proxy_response
-            (
-                path, method, data_key, status_code, header, body, attachment_file, file_name
-            )
-        VALUES
-            (
-                #{path}, UPPER(#{method}), IFNULL(#{dataKey},''), #{statusCode}, #{header}, #{body}, #{attachmentFile}, #{fileName}
-            )
-    ''')
-    void createProxyResponse(ApiResponse mockResponse)
+  @Insert('''
+    INSERT INTO api_proxy_response
+      (
+        path, method, data_key, status_code, header, body, attachment_file, file_name
+      )
+    VALUES
+      (
+        #{path}, UPPER(#{method}), IFNULL(#{dataKey},''), #{statusCode}, #{header}, #{body}, #{attachmentFile}, #{fileName}
+      )
+  ''')
+  void createProxyResponse(ApiResponse mockResponse)
 
-    @Update('''
-        UPDATE mock_api_response
-        SET
-            data_key = IFNULL(#{dataKey},''), status_code = #{statusCode}, header = #{header}
-            , body = #{body}, body_editor_mode = #{bodyEditorMode}, attachment_file = #{attachmentFile}
-            , file_name = #{fileName}, waiting_msec = #{waitingMsec}, description = #{description}
-        WHERE
-            id = #{id}
-    ''')
-    void update(ApiResponse mockResponse)
+  @Update('''
+    UPDATE mock_api_response
+    SET
+      data_key = IFNULL(#{dataKey},''), status_code = #{statusCode}, header = #{header}
+      , body = #{body}, body_editor_mode = #{bodyEditorMode}, attachment_file = #{attachmentFile}
+      , file_name = #{fileName}, waiting_msec = #{waitingMsec}, description = #{description}
+    WHERE
+      id = #{id}
+  ''')
+  void update(ApiResponse mockResponse)
 
-    @Delete('''
-        DELETE FROM
-            mock_api_response
-        WHERE
-            id = #{id}
-    ''')
-    void delete(int id);
+  @Delete('''
+    DELETE FROM
+      mock_api_response
+    WHERE
+      id = #{id}
+  ''')
+  void delete(int id);
 
-    @Delete('''
-        DELETE FROM
-            mock_api_response_history
-        WHERE
-            id = #{id}
-        AND
-            sub_id = #{subId}
-    ''')
-    void deleteHistory(@Param("id") int id, @Param("subId") int subId)
+  @Delete('''
+    DELETE FROM
+      mock_api_response_history
+    WHERE
+      id = #{id}
+    AND
+      sub_id = #{subId}
+  ''')
+  void deleteHistory(@Param("id") int id, @Param("subId") int subId)
 
-    @Delete('''
-        DELETE FROM
-            mock_api_response_history
-        WHERE
-            id = #{id}
-    ''')
-    void deleteAllHistory(int id);
+  @Delete('''
+    DELETE FROM
+      mock_api_response_history
+    WHERE
+      id = #{id}
+  ''')
+  void deleteAllHistory(int id);
 
-    static class SqlProvider {
-        public String findPage(
-                @Param("path") String path, @Param("method") String method, @Param("description") String description) {
-            return new SQL() {
-                {
-                    SELECT("id", "path", "method", "data_key", "status_code", "description")
-                    FROM("mock_api_response")
-                    where(this, path, method, description)
-                    ORDER_BY("path", "method", "data_key")
-                }
-            }.toString()
+  static class SqlProvider {
+    public String findPage(
+        @Param("path") String path, @Param("method") String method, @Param("description") String description) {
+      return new SQL() {
+        {
+          SELECT("id", "path", "method", "data_key", "status_code", "description")
+          FROM("mock_api_response")
+          where(this, path, method, description)
+          ORDER_BY("path", "method", "data_key")
         }
-        public String count(
-                @Param("path") String path, @Param("method") String method, @Param("description") String description) {
-            return new SQL() {
-                {
-                    SELECT("COUNT(*)")
-                    FROM("mock_api_response")
-                    where(this, path, method, description)
-                }
-
-            }.toString()
-        }
-        private static void where(SQL sql,  String path, String method, String description) {
-            if (StringUtils.hasLength(path)) {
-                sql.WHERE("path REGEXP #{path}")
-            }
-            if (StringUtils.hasLength(method)) {
-                sql.WHERE("method = UPPER(#{method})")
-            }
-            if (StringUtils.hasLength(description)) {
-                sql.WHERE("description REGEXP #{description}")
-            }
-        }
+      }.toString()
     }
+
+    public String count(
+        @Param("path") String path, @Param("method") String method, @Param("description") String description) {
+      return new SQL() {
+        {
+          SELECT("COUNT(*)")
+          FROM("mock_api_response")
+          where(this, path, method, description)
+        }
+
+      }.toString()
+    }
+
+    private static void where(SQL sql, String path, String method, String description) {
+      if (StringUtils.hasLength(path)) {
+        sql.WHERE("path REGEXP #{path}")
+      }
+      if (StringUtils.hasLength(method)) {
+        sql.WHERE("method = UPPER(#{method})")
+      }
+      if (StringUtils.hasLength(description)) {
+        sql.WHERE("description REGEXP #{description}")
+      }
+    }
+  }
 
 }
