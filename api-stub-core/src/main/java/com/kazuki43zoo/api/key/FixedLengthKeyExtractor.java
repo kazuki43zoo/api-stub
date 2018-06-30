875e4b35aeba8274430f86daf5cfbf08fb885b94
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-06-30 20:33:47 +0900

    Polishing

--- api-stub-core/src/main/java/com/kazuki43zoo/api/key/FixedLengthKeyExtractor.java
+++ api-stub-core/src/main/java/com/kazuki43zoo/api/key/FixedLengthKeyExtractor.java
@@ -41,18 +41,18 @@ import java.util.stream.Stream;
 @Order(3)
 public class FixedLengthKeyExtractor implements KeyExtractor {
 
-  private static final Map<String, BiFunction<byte[], Charset, Object>> BYTES_TO_OBJECT_FUNCTIONS;
+  private static final Map<String, BiFunction<byte[], Charset, Object>> FUNCTIONS;
 
   static {
-    Map<String, BiFunction<byte[], Charset, Object>> bytesToObjectFunctions = new HashMap<>();
-    bytesToObjectFunctions.put("string", String::new);
-    bytesToObjectFunctions.put("short", (bytes, charset) -> ByteBuffer.wrap(bytes).getShort());
-    bytesToObjectFunctions.put("int", (bytes, charset) -> ByteBuffer.wrap(bytes).getInt());
-    bytesToObjectFunctions.put("long", (bytes, charset) -> ByteBuffer.wrap(bytes).getLong());
-    bytesToObjectFunctions.put("char", (bytes, charset) -> ByteBuffer.wrap(bytes).getChar());
-    bytesToObjectFunctions.put("float", (bytes, charset) -> ByteBuffer.wrap(bytes).getFloat());
-    bytesToObjectFunctions.put("double", (bytes, charset) -> ByteBuffer.wrap(bytes).getDouble());
-    BYTES_TO_OBJECT_FUNCTIONS = Collections.unmodifiableMap(bytesToObjectFunctions);
+    Map<String, BiFunction<byte[], Charset, Object>> functions = new HashMap<>();
+    functions.put("string", String::new);
+    functions.put("short", (bytes, charset) -> ByteBuffer.wrap(bytes).getShort());
+    functions.put("int", (bytes, charset) -> ByteBuffer.wrap(bytes).getInt());
+    functions.put("long", (bytes, charset) -> ByteBuffer.wrap(bytes).getLong());
+    functions.put("char", (bytes, charset) -> ByteBuffer.wrap(bytes).getChar());
+    functions.put("float", (bytes, charset) -> ByteBuffer.wrap(bytes).getFloat());
+    functions.put("double", (bytes, charset) -> ByteBuffer.wrap(bytes).getDouble());
+    FUNCTIONS = Collections.unmodifiableMap(functions);
   }
 
   @Override
@@ -67,25 +67,25 @@ public class FixedLengthKeyExtractor implements KeyExtractor {
         .orElse(StandardCharsets.UTF_8);
 
     return Stream.of(expressions).map(expression -> {
-      String[] extractionDefine = StringUtils.splitByWholeSeparatorPreserveAllTokens(expression, ",");
-      if (extractionDefine.length <= 2) {
+      String[] defines = StringUtils.splitByWholeSeparatorPreserveAllTokens(expression, ",");
+      if (defines.length <= 2) {
         return null;
       }
-      int offset = Integer.parseInt(extractionDefine[0].trim());
-      int length = Integer.parseInt(extractionDefine[1].trim());
-      String type = extractionDefine[2].trim().toLowerCase();
+      int offset = Integer.parseInt(defines[0].trim());
+      int length = Integer.parseInt(defines[1].trim());
+      String type = defines[2].trim().toLowerCase();
       final Charset charset;
-      if (extractionDefine.length >= 4) {
-        charset = Charset.forName(extractionDefine[3].trim());
+      if (defines.length >= 4) {
+        charset = Charset.forName(defines[3].trim());
       } else {
         charset = defaultCharset;
       }
       if (!(requestBody.length >= offset && requestBody.length >= offset + length)) {
         return null;
       }
-      return Optional.ofNullable(BYTES_TO_OBJECT_FUNCTIONS.get(type))
+      return Optional.ofNullable(FUNCTIONS.get(type))
           .orElseThrow(() -> new IllegalArgumentException("A bad expression is detected. The specified type does not support. expression: '"
-              + expression + "', specified type: '" + type + "', allowing types: " + BYTES_TO_OBJECT_FUNCTIONS.keySet()))
+              + expression + "', specified type: '" + type + "', allowing types: " + FUNCTIONS.keySet()))
           .apply(Arrays.copyOfRange(requestBody, offset, offset + length), charset);
     }).filter(Objects::nonNull).collect(Collectors.toList());
   }
